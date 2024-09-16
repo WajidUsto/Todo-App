664eb47880a17b9bba87c7fbcf62d0d3af517b98
Wajid Usto <wajidusto@gmail.com>
2024-09-17 00:35:14 +0500

    command added

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
 
-// let input = document.querySelector("#input")
-// let btn = document.querySelector("#btn")
-// let ol = document.querySelector("#ol")
-// let userArr = []
 
-// btn.addEventListener("click" , function total() {
-//     userArr.push(input.value);
-//     console.log(userArr);
-//     input.value = ""
-
-//     for(let i = 0; i < userArr.length; i++){
-//         ol.innerHtml = `<li>${userArr [i]}</li>`
-//     }
-// })
+let todoArr = []
+let input = document.querySelector(".input")
+let btn = document.querySelector("button")
+let div = document.querySelector('.main');
 
 
+function todo() {
+    div.innerHTML = ""
+    for(let i = 0; i < todoArr.length; i++){
+        div.innerHTML += `s
+        <div class = "list">
+        <h2 class = "h2">${todoArr[i]}</h2>
+        <div class = "icons">
+        <div onclick = "deleteTodo(${i})">
+        <i class="fa-solid fa-2 fa-trash"></i>
+        </div>
+        <div onclick = "editTodo(${i})">
+        <i class="fa-solid fa-1 fa-pen-to-square"></i>
+        </div>
+        </div>
+        </div>
+        `
+    }
+}
 
-
-
-
+function addTodo() {
+    todoArr.push(input.value)
+    console.log(todoArr);
+    input.value = ""
+    todo()
+}
+function deleteTodo(index) {
+    todoArr.splice(index , 1)
+    console.log(todoArr);
+    todo()
+}
+function editTodo(index) {
+    let edit = prompt("Enter Updated Value" , todo[index])
+    todoArr.splice(index , 1 , edit)
+    console.log(todoArr);
+    todo()
+}
 
 
 
