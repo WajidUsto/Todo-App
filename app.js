d9662e4d186f17987bf860df037a2da7170dfc28
Wajid Usto <wajidusto@gmail.com>
2024-09-16 16:45:43 +0500

    command added

---
+++ app.js
@@ -0,0 +1,12 @@
+
+
+let input = document.querySelector("#input")
+let btn = document.querySelector("#btn")
+
+let userArr = []
+
+btn.addEventListener("click" , function total() {
+    console.log(`user Arrey ${userArr = input.value}`);
+    console.log(`User Value ${input.value}`);
+    input.value = ""
+})
